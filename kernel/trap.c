95fdc6cf37d02e36f72afc6bca1bd12e28e250f6
jingzeLiu <1800012818@pku.edu.cn>
2021-09-28 14:57:33 +0800

    score 119/119

--- kernel/trap.c
+++ kernel/trap.c
@@ -65,9 +65,32 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  }else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  }else if(r_scause() == 13 || r_scause() == 15){ 
+    uint64 va = r_stval();
+    if (va >= p->sz || va < p->trapframe->sp)
+    {
+      p->killed = 1;
+    }else
+    {
+      char *mem;
+      mem = kalloc();
+      if(mem == 0)
+      {
+        p->killed = 1;
+      }else
+      {
+        memset(mem, 0, PGSIZE);
+        va = PGROUNDDOWN(va);
+        if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0)
+        {
+          kfree(mem);
+          p->killed = 1;
+        }
+      }
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
